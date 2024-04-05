ce865186cb2e23cb899e55cbe71387bfe40cc407
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-06 01:41:24 +0530

    showing posts

---
+++ src/slices/ContentIndex/ContentList.tsx
@@ -0,0 +1,41 @@
+import { Content } from "@prismicio/client";
+import React from "react";
+
+type ContentListProps = {
+  items: Content.BlogPostDocument[] | Content.ProjectDocument[];
+  contentType: Content.ContentIndexSlice["primary"]["content_type"];
+  fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
+  viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"];
+};
+
+export default function ContentList({
+  items,
+  contentType,
+  fallbackItemImage,
+  viewMoreText = "Read More",
+}: ContentListProps) {
+  return (
+    <div>
+      <ul className="grid border-b border-b-slate-100">
+        {items.map((item, index) => (
+          <li className="list-item opacity-0f">
+            <a
+              href=""
+              className="flex flex-col justify-between border-t border-t-slate-100 py-10  text-slate-200 md:flex-row "
+            >
+              <div className="flex flex-col">
+                <span>{item.data.title}</span>
+                <div>
+                  {item.tags.map((tag, index) => (
+                    <span key={index}>{tag}</span>
+                  ))}
+                </div>
+              </div>
+              <span>{viewMoreText}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
