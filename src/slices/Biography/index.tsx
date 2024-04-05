50b53d5e6e4bba485fe08ce01b30f6e22147338a
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-05 23:36:59 +0530

    typography

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -1,6 +1,7 @@
 import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `Biography`.
@@ -16,7 +17,14 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for biography (variation: {slice.variation}) Slices
+      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
+      <Heading size="xl" className="col-start-1">
+          {slice.primary.heading}
+        </Heading>
+        <div className="prose prose-xl prose-slate prose-invert col-start-1">
+        <PrismicRichText field={slice.primary.description} />
+        </div>
+      </div>
     </Bounded>
   );
 };
