633cf45a67b038fe719504a01676f9ea2e2fb178
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-05 23:49:20 +0530

    About page loading

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -1,6 +1,8 @@
 import Bounded from "@/components/Bounded";
+import Button from "@/components/Button";
 import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
@@ -24,6 +26,8 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
         <div className="prose prose-xl prose-slate prose-invert col-start-1">
         <PrismicRichText field={slice.primary.description} />
         </div>
+        <Button linkField={slice.primary.button_link} label={slice.primary.button_text}></Button>
+        <PrismicNextImage field={slice.primary.avatar} className="row-start-1 max-w-sm md:col-start-2 md:row-end-3"/>
       </div>
     </Bounded>
   );
