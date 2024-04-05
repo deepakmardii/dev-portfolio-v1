59f631dfee056193b6d1e28c0c0819319445680e
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-05 23:30:55 +0530

    Header

---
+++ src/slices/Biography/index.tsx
@@ -0,0 +1,24 @@
+import Bounded from "@/components/Bounded";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Biography`.
+ */
+export type BiographyProps = SliceComponentProps<Content.BiographySlice>;
+
+/**
+ * Component for "Biography" Slices.
+ */
+const Biography = ({ slice }: BiographyProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      Placeholder component for biography (variation: {slice.variation}) Slices
+    </Bounded>
+  );
+};
+
+export default Biography;
